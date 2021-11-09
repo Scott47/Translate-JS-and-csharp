50330c24e447b8270a4bde5a64f72b255dee544d
scottsilver <scottsilver47@gmail.com>
2021-11-09 14:05:30 -0600

    files added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace translate_csharp_js
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
