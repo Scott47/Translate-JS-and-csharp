695f9a175f9eb95aadec6460e3f35c464e57d3e4
scottsilver <scottsilver47@gmail.com>
2021-11-09 16:39:43 -0600

    added logic to enemies-list js to get same output as enemies-list cs

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,57 @@
 // Put your code here
+const getEnemiesList = () => [
+    {
+        FirstName: "Joshua",
+        LastName: "Flowers",
+        Offenses: [
+            "Being a jerk to me in elementary school",
+            "Not being nice to me in elementary school"
+        ],
+        IsReallyHated: true
+    },
+    {
+        FirstName: "Darth",
+        LastName: "Vader",
+        Offenses: [
+            "Cut off Luke's hand",
+            "Murd]red all those kids",
+            "Unkind management practices"
+        ],
+        IsReallyHated: false
+    },
+    {
+        FirstName: "Betty",
+        LastName: "Rudelady",
+        Offenses: [
+            "Phone calls in the theater",
+            "Phone calls on the bus",
+            "Phone calls in line at the grocery store",
+            "Poor conversationalist"
+        ],
+        IsReallyHated: true
+    },
+    {
+        FirstName: "Leon",
+        LastName: "Peck",
+        Offenses: [
+            "Keeps giving me a hotplate"
+        ],
+        IsReallyHated: false
+    }
+]
+
+const Main = () => {
+    const listOfEnemies = getEnemiesList()
+    console.log("My Enemies List!")
+    console.log("----------------")
+    listOfEnemies.map(enemy => {
+        if (enemy.IsReallyHated) {
+            console.log(`${enemy.FirstName} ${enemy.LastName} (Really, really dislike!)`)
+        } else {
+            console.log(`${enemy.FirstName} ${enemy.LastName}`)
+        }
+
+    })
+}
+
+Main()
