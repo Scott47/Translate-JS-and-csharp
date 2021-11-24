b895cd15d24a1f9e14a1f67ffb67685f030ddbcb
scottsilver <scottsilver47@gmail.com>
2021-11-24 10:18:11 -0600

    completed wishlist js to c# translation

--- dinner-party/csharp/dinner-party.cs
+++ dinner-party/csharp/dinner-party.cs
@@ -50,7 +50,6 @@ namespace Guests
         }
         public string name { get; set; }
         public string occupation { get; set; }
-
         public string bio { get; set; }
     }
 }
--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,42 +1,71 @@
 // Put your code here
-const getAllSpells = () => {
-    const spells = [
-        {
-            Name: "Turn enemy into a newt",
-            IsEvil: true,
-            EnergyReqired: 5.1
-        },
-        {
-            Name: "Conjure some gold for a local charity",
-            IsEvil: false,
-            EnergyReqired: 2.99
-        },
-        {
-            Name: "Give a deaf person the ability to heal",
-            IsEvil: false,
-            EnergyReqired: 12.2
-        },
-        {
-            Name: "Make yourself emperor of the universe",
-            IsEvil: true,
-            EnergyReqired: 100.0
-        },
-        {
-            Name: "Convince your relatives your political views are correct",
-            IsEvil: false,
-            EnergyReqired: 2921.5
-        }
-    ]
-    return spells
+console.log("Do you believe in magic?")
+console.log("------------------------")
+
+const SpellBook = (title, spells) => {
+    return {
+        title: title,
+        spells: spells
+    }
 }
 
-const makeGoodSpellBook = () => {
-    const spells = getAllSpells()
-    const goodSpells = spells.filter(spell => !spell.IsEvil)
-    return goodSpells
+const Spell = (name, isEvil, energyRequired) => {
+    return {
+        name: name,
+        isEvil: isEvil,
+        energyRequired: energyRequired
+    }
 }
-const makeEvilSpellBook = () => {
-    const spells = getAllSpells()
-    const evilSpells = spells.filter(spell => spell.IsEvil)
-    return evilSpells
+
+const allSpells = [
+    {
+        name: "Turn enemy into a newt",
+        isEvil: true,
+        energyRequired: 5.1
+    },
+    {
+        name: "Conjure some gold for a local charity",
+        isEvil: false,
+        energyRequired: 2.99
+    },
+    {
+        name: "Give a deaf person the ability to hear",
+        isEvil: false,
+        energyRequired: 12.2
+    },
+    {
+        name: "Make yourself emperor of the universe",
+        isEvil: true,
+        energyRequired: 100.0
+    },
+    {
+        name: "Convince your relatives your political views are correct",
+        isEvil: false,
+        energyRequired: 2921.5
+    }
+]
+
+const DisplaySpellBook = (book) => {
+    console.log(book.title)
+    for (const aSpell of book.spells) {
+        console.log(aSpell.name)
+    }
 }
+
+const MakeEvilSpellBook = (allSpells) => {
+    const evilBook = SpellBook("Evil Book", allSpells.filter(spell => spell.isEvil))
+    return evilBook
+}
+
+const MakeGoodSpellBook = (allSpells) => {
+    const goodBook = SpellBook("Good Book", allSpells.filter(spell => !spell.isEvil))
+    return goodBook
+}
+
+const goodBook = MakeGoodSpellBook(allSpells)
+const evilBook = MakeEvilSpellBook(allSpells)
+
+DisplaySpellBook(goodBook)
+console.log("")
+DisplaySpellBook(evilBook)
+
